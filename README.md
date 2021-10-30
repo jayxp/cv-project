6c92ccdb83ec1d10b026ccc8f58f699aac79af76
Jalil Patten <pattenjaym@gmail.com>
2021-10-30 13:37:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# React Resume Builder
+
+---
+
+## :page_with_curl: Key Features
+
+#### 1.
+
+#### 2.
+
+#### 3.
+
+## :key: Usage
+
+`Stuff here`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h3>Something</h3>
+    </div>
+  );
+}
+
+export default App;
