0a24e4da41d3bf913a30fa27f46830f6531ba2ee
Jalil Patten <pattenjaym@gmail.com>
2021-10-31 06:54:00 -0500

    Defined basic project structure and dataflow in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from "react";
+import uniqid from "uniqid";
 
-function App() {
-  return (
-    <div>
-      <h3>Something</h3>
-    </div>
-  );
+const CATEGORIES = [
+  { type: "Header" },
+  { type: "Education" },
+  { type: "Employment" },
+  { type: "Projects" },
+  { type: "Technical Skills" },
+];
+
+function Resume({ categories }) {
+  function Section({ category }) {
+    function Listings({ type }) {
+      return (
+        <div>
+          <h4>Stuff here</h4>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>{category.type}</h2>
+        <hr />
+        <Listings type={category.type} />
+      </div>
+    );
+  }
+
+  return categories.map((category) => {
+    return <Section key={uniqid()} category={category} />;
+  });
 }
 
-export default App;
+export default function App() {
+  return <Resume categories={CATEGORIES} />;
+}
+
+/* 
+
+Sections are the parent of Listings
+1. 5 section types: Header, Education, Employment,
+Projects, & Technical Skills
+2. Function for deleting listings needs to be defined in Section
+and passed to child Listings
+
+
+*/
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Sections() {
+export default function Resume() {
   return null;
 }
