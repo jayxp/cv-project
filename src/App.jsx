9f21dbe17e301a405ec14534e2d12a73db5cd584
Jalil Patten <pattenjaym@gmail.com>
2021-11-11 18:04:53 -0600

    Replaced spreads with destructuring, added top-level unique ID for list rendering, added ref to Education component, added additional state management to Education component, added conditional rendering to Input component, populated initial attribute values for all datastores, added conditional rendering to Education component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import uniqid from "uniqid";
+
+/* 
+============ TO DO ============
+[] Add submit handler to remaining components
+[] Add deletion logic for appropriate components
+[] Add edit logic to all components
+[] Create sub-list logic for appropriate components
+[] Make the app cute
+
+============ REMINDER ============
+- May need to add edit attribute to datastores
+- Possibly do something with fragmented returns
+- Think of a better variable name than list, confusing when accessing array indices
+- disable eslint prop validation & function binding rules
+- Reactstrap or Styled Components? Research
+*/
 
 const headerData = {
   email: {
@@ -41,51 +57,135 @@ const headerData = {
 
 const educationData = {
   eduList: [],
+  id: uniqid(),
   location: {
     text: "",
-    name: "",
+    name: "location",
     type: "text",
-    description: "",
-    example: "",
+    description: "Location",
+    example: "City, State",
   },
   college: {
     text: "",
-    name: "",
+    name: "college",
     type: "text",
-    description: "",
-    example: "",
+    description: "College",
+    example: "University of X",
   },
-  date: {
+  dateFrom: {
     text: "",
-    name: "",
-    type: "text",
-    description: "",
-    example: "",
+    name: "dateFrom",
+    type: "month",
+    description: "Date From",
+    example: "2014-08",
+  },
+  dateTo: {
+    text: "",
+    name: "dateTo",
+    type: "month",
+    description: "Date To",
+    example: "2018-05",
   },
   degree: {
     text: "",
-    name: "",
+    name: "degree",
     type: "text",
-    description: "",
-    example: "",
+    description: "Degree Program",
+    example: "BS in X",
   },
 };
 
-const employmentData = {};
+const employmentData = {
+  employmentList: [],
+  id: uniqid(),
+  job: {
+    text: "",
+    name: "job",
+    type: "text",
+    description: "Position",
+    example: "Software Developer",
+  },
+  company: {
+    text: "",
+    name: "company",
+    type: "text",
+    description: "Company Name",
+    example: "Google",
+  },
+  dateFrom: {
+    text: "",
+    name: "dateFrom",
+    type: "month",
+    description: "Date From",
+    example: "2018-06",
+  },
+  dateTo: {
+    text: "",
+    name: "dateTo",
+    type: "month",
+    description: "Date To",
+    example: "2020-08",
+  },
+  tasks: [],
+};
 
-const projectData = {};
+const projectData = {
+  projectList: [],
+  id: uniqid(),
+  pName: {
+    text: "",
+    name: "pName",
+    type: "text",
+    description: "Project Name",
+    example: "Example Project",
+  },
+  link: {
+    text: "",
+    name: "link",
+    type: "text",
+    description: "Project URL",
+    example: "https://github.com/user/link",
+  },
+  details: [],
+};
 
-const technicalData = {};
+const technicalData = {
+  languages: {
+    text: "",
+    name: "languages",
+    type: "text",
+    description: "",
+    example: "HTML, CSS, JavaScript, SQL, Python",
+  },
+  frameLibs: {
+    text: "",
+    name: "frameLibs",
+    type: "text",
+    description: "",
+    example: "React, React-Native, Node.js, Express.js",
+  },
+  tools: {
+    text: "",
+    name: "tools",
+    type: "text",
+    description: "",
+    example: "Git, Linux, Webpack, NPM, Jira",
+  },
+};
 
 const Input = (props) => {
-  const { text, name, type, description, example } = props.data;
-
+  const { data } = props;
+  const { text, name, type, description, example } = data;
   const handleChange = (event) => props.onChange(event);
 
   return (
     <>
-      <label htmlFor={text}>{description}</label>
-      <br />
+      {description !== "" && (
+        <>
+          <label htmlFor={text}>{description}</label>
+          <br />
+        </>
+      )}
       <input
         value={text}
         name={name}
@@ -98,10 +198,18 @@ const Input = (props) => {
 };
 
 function HeaderCategoryRow(props) {
-  const [listing, setListing] = useState({ ...props });
+  const { data } = props;
+  const { email, phone, name, website, github } = data;
+  const [entry, setEntry] = useState({
+    email,
+    phone,
+    name,
+    website,
+    github,
+  });
 
   function handleChange(event) {
-    setListing((prevState) => {
+    setEntry((prevState) => {
       return {
         ...prevState,
         [event.target.name]: {
@@ -114,105 +222,227 @@ function HeaderCategoryRow(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    setListing({ edit: false });
+    setEntry({ edit: false });
   }
-
-  /* Current form can likely be extracted into its own component */
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <Input data={listing.email} onChange={handleChange} />
+        <Input data={entry.email} onChange={handleChange} />
         <br />
-        <Input data={listing.phone} onChange={handleChange} />
+        <Input data={entry.phone} onChange={handleChange} />
         <br />
-        <Input data={listing.name} onChange={handleChange} />
+        <Input data={entry.name} onChange={handleChange} />
         <br />
-        <Input data={listing.website} onChange={handleChange} />
+        <Input data={entry.website} onChange={handleChange} />
         <br />
-        <Input data={listing.github} onChange={handleChange} />
+        <Input data={entry.github} onChange={handleChange} />
       </form>
     </>
   );
 }
 
-function EducationCategoryRow() {
-  const [listing, setListing] = useState({});
+function EducationCategoryRow(props) {
+  const { data } = props;
+  const { eduList, id, location, college, dateFrom, dateTo, degree } = data;
+  const [entry, setEntry] = useState({
+    id,
+    location,
+    college,
+    dateFrom,
+    dateTo,
+    degree,
+  });
+  const [list, setList] = useState({ eduList });
+  const initialEntry = useRef({
+    location,
+    college,
+    dateFrom,
+    dateTo,
+    degree,
+  });
+
+  function handleChange(event) {
+    setEntry((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: {
+          ...prevState[event.target.name],
+          text: event.target.value,
+        },
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setList({ eduList: list.eduList.concat(entry) });
+    setEntry({
+      id: uniqid(),
+      ...initialEntry.current,
+    });
+  }
 
   return (
-    <div>
-      <h2>Education</h2>
-      <hr />
-      <p>{listing.location}</p>
-      <p>{listing.college}</p>
-      <p>{listing.date}</p>
-      <p>{listing.degree}</p>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Education</h2>
+        <hr />
+        <Input data={entry.location} onChange={handleChange} />
+        <br />
+        <Input data={entry.college} onChange={handleChange} />
+        <br />
+        <Input data={entry.dateFrom} onChange={handleChange} />
+        <br />
+        <Input data={entry.dateTo} onChange={handleChange} />
+        <br />
+        <Input data={entry.degree} onChange={handleChange} />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+        <br />
+      </form>
+      {list.eduList.length > 0 &&
+        list.eduList.map((e) => {
+          return (
+            <div key={e.id}>
+              <p>{e.location.text}</p>
+              <p>{e.college.text}</p>
+              <p>{e.dateFrom.text}</p>
+              <p>{e.dateTo.text}</p>
+              <p>{e.degree.text}</p>
+            </div>
+          );
+        })}
+    </>
   );
 }
 
-function EmploymentCategoryRow() {
-  const [listing, setListing] = useState({
-    job: "Position",
-    company: "Company Name",
-    date: "From -- To",
-    tasks: [],
+function EmploymentCategoryRow(props) {
+  const { data } = props;
+  const { employmentList, id, job, company, dateFrom, dateTo, tasks } = data;
+  const [entry, setEntry] = useState({
+    id,
+    job,
+    company,
+    dateFrom,
+    dateTo,
+    tasks,
   });
 
+  function handleChange(event) {
+    setEntry((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: {
+          ...prevState[event.target.name],
+          text: event.target.value,
+        },
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setEntry({ edit: false });
+  }
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h2>Employment</h2>
       <hr />
-      <p>{listing.job}</p>
-      <p>{listing.company}</p>
-      <p>{listing.date}</p>
-    </div>
+      <Input data={entry.job} onChange={handleChange} />
+      <br />
+      <Input data={entry.company} onChange={handleChange} />
+      <br />
+      <Input data={entry.dateFrom} onChange={handleChange} />
+      <br />
+      <Input data={entry.dateTo} onChange={handleChange} />
+    </form>
   );
 }
 
-function ProjectCategoryRow() {
-  const [listing, setListing] = useState({
-    name: "Project Name",
-    link: "Project Link",
-    details: [],
-  });
+function ProjectCategoryRow(props) {
+  const { data } = props;
+  const { projectList, id, pName, link, details } = data;
+  const [entry, setEntry] = useState({ id, pName, link, details });
+
+  function handleChange(event) {
+    setEntry((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: {
+          ...prevState[event.target.name],
+          text: event.target.value,
+        },
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setEntry({ edit: false });
+  }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h2>Projects</h2>
       <hr />
-      <p>{listing.name}</p>
-      <p>{listing.link}</p>
-    </div>
+      <Input data={entry.pName} onChange={handleChange} />
+      <br />
+      <Input data={entry.link} onChange={handleChange} />
+    </form>
   );
 }
 
-function TechnicalCategoryRow() {
-  const [listing, setListing] = useState({
-    langs: [],
-    framelibs: [],
-    tools: [],
-  });
+function TechnicalCategoryRow(props) {
+  const { data } = props;
+  const { languages, frameLibs, tools } = data;
+  const [entry, setEntry] = useState({ languages, frameLibs, tools });
+
+  function handleChange(event) {
+    setEntry((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: {
+          ...prevState[event.target.name],
+          text: event.target.value,
+        },
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setEntry({ edit: false });
+  }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h2>Technical Skills</h2>
       <hr />
-      <p>Languages:</p>
-      <p>Frameworks and Libraries:</p>
-      <p>Tools:</p>
-    </div>
+      <p>
+        Languages: <Input data={entry.languages} onChange={handleChange} />
+      </p>
+      <p>
+        Frameworks and Libraries:{" "}
+        <Input data={entry.frameLibs} onChange={handleChange} />
+      </p>
+      <p>
+        Tools: <Input data={entry.tools} onChange={handleChange} />
+      </p>
+    </form>
   );
 }
 
 export default function App() {
   return (
     <div>
-      <HeaderCategoryRow {...headerData} />
-      <EducationCategoryRow />
-      <EmploymentCategoryRow />
-      <ProjectCategoryRow />
-      <TechnicalCategoryRow />
+      <HeaderCategoryRow data={headerData} />
+      <EducationCategoryRow data={educationData} />
+      <EmploymentCategoryRow data={employmentData} />
+      <ProjectCategoryRow data={projectData} />
+      <TechnicalCategoryRow data={technicalData} />
     </div>
   );
 }
